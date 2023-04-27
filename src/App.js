4315083019740da4f5e0ce45aa37f78521775d8a
AndreyNedelevich <nedelevich@gmail.com>
2023-04-27 11:08:26 +0300

    lesson_5_useReducerPart2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import {Cars} from "./components/Cars/Cars";
-import {CarForm} from "./components/CarForm/CarForm";
+import './App.css';
+import FormNewAnimal from "./components/newAnimal/FormNewAnimal";
 
-const App = () => {
-    return (
-        <div>
-            <CarForm/>
-            <Cars/>
-        </div>
-    );
-};
+function App() {
+
+
+  // Використовуючи хук useReducer в якому початковый стейт буде {cats: [], dogs:[]}
+  // Реалізвути збереження окремо нового кота, та окремо собаку (приклад на зображенні)
+  //
+  // Advance
+  // реалізувати видалення для кожного при натисканні на кнопку delete
+
+
+
+  return (
+    <div >
+      <FormNewAnimal/>
+    </div>
+  );
+}
 
 export default App;
