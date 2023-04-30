9bdf9d04a4c479616548d2385e5363ce3252b701
AndreyNedelevich <nedelevich@gmail.com>
2023-04-30 11:51:34 +0300

    lesson_6_redux_base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {reset, increment, decrement} from "./redux";
 
-function App() {
-  return (
-    <div >
 
-    </div>
-  );
-}
+const App = () => {
+
+    let store = useSelector(state => state);
+    let dispatch = useDispatch();
+
+    const onincrement = () => {
+        dispatch(increment());
+
+    };
+    const ondecrement = () => {
+        dispatch(decrement());
+    };
+    const onres = () => {
+        dispatch(reset());
+    };
+    return (
+        <div>
+
+            <h1>{store}</h1>
+            <button onClick={onincrement}>increment</button>
+            <button onClick={ondecrement}>decrement</button>
+            <button onClick={onres}>reset</button>
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {store} from "./redux";
+
 import App from './App';
+import {Provider} from "react-redux";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
