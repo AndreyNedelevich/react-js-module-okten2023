cec9cdd61af3f2d760efd5ad9b94d21cb0fad6f6
AndreyNedelevich <nedelevich@gmail.com>
2023-04-30 11:39:40 +0300

    lesson_6_redux_base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div >
 
-    </div>
-  );
+const fetchCars = () => async (dispatch) => {
+
+    dispatch({type: 'START_LOADING'});
+    let response = await fetch('http://owu.linkpc.net/carsAPI/v1/cars')
+        .then(value => value.json())
+    console.log(response);
+    dispatch({type: 'SET_CARS', payload: response});
+
+
 }
+
+const App = () => {
+
+    let store = useSelector(state => state);
+    let dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCars());
+        //.....
+
+    }, []);
+
+
+    return (
+        <div>
+            {store.isLoading && <h2>loading...</h2>}
+
+            {
+                store.cars.map(value => <div key={value.id}>{value.brand}</div>)
+            }
+
+        </div>
+    );
+};
 
 export default App;
